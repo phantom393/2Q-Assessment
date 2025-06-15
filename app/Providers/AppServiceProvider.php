33d0cba0af23c047432b86db13af82e5c0b06891
naim <azrul393@gmail.com>
2025-06-15 21:27:34 +0800

    u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use Livewire\Mechanisms\FrontendAssets\FrontendAssets;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
