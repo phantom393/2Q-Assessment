22d64118d3916a9184439efeb81b3f0dcffe457a
naim <azrul393@gmail.com>
2025-06-17 19:05:09 +0800

    Update company-manager.blade.php

--- resources/views/livewire/company-manager.blade.php
+++ resources/views/livewire/company-manager.blade.php
@@ -3,6 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <form wire:submit.prevent="submit" enctype="multipart/form-data">
+                     @csrf
                     <div class="modal-header">
                         <h5 class="modal-title" id="companyModalLabel">
                             {{ $isEditing ? 'Edit Company' : 'Add Company' }}
