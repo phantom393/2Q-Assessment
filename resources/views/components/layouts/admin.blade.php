da2f198c0d17869c6b28786d594c8b9e5a17f8cb
naim <azrul393@gmail.com>
2025-06-17 18:52:14 +0800

    u

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>2Q</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite('resources/css/app.css')
     @livewireStyles
 </head>
 <body style="font-size: 12px;">
@@ -13,6 +14,7 @@
         {{ $slot }}
     </main>
     @livewireScripts
+    @vite('resources/js/app.js')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
