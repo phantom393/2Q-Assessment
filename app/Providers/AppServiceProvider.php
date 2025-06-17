a87061491694d9bd70b421ef1b04412afc29329a
naim <azrul393@gmail.com>
2025-06-17 18:32:28 +0800

    u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,13 +22,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('local')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-
-            // This will tell Livewire where to load its script from (your ngrok URL)
-            Livewire::setScriptRoute(function () {
-                return 'https://624d-118-101-168-70.ngrok-free.app/livewire/livewire.js';
-            });
-        }
     }
 }
