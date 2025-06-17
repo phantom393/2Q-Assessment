c348e6d75531b24383a10d66e01a1aea3505b2fe
naim <azrul393@gmail.com>
2025-06-17 18:37:28 +0800

    u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+        }
     }
 }
